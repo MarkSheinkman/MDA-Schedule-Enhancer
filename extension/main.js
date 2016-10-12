8a68bf42570f03b2812d9cbd4648423e5aba9ee2
Mark <mrk.sheinkman@gmail.com>
2016-10-12 22:02:36 +0300

    color days where user has shifts

--- extension/main.js
+++ extension/main.js
@@ -13,27 +13,24 @@ $(()=>{
   function getRowDay(row){  
     return dayHebToNum[$(row).find("td:not([class])").slice(1,2).text()];
   }
-
-  // $("tr[data-rownum]")
-  //     .filter((rowi,row) => [6,7].includes(getRowDay(row)))
-  //     .each((rowi, row) => $(row).css("background-color","lightblue"));
-
+  
   // mark weekends
   $("tr[data-rownum]")
     .filter((rowi,row) => [7].includes(getRowDay(row)))
-    .each((rowi, row) => $(row).css("border-bottom","3px solid black"));
+    .css("border-bottom","3px solid black");
 
   // mark available slots
   $("tr[data-rownum]>td")
     .filter((i,d) => d.innerText == slotAvailableText)
-    .each((i,d) => $(d).css("font-weight","bold"));
+    .css("font-weight","bold");
 
   // seperate stations
- $("tr[data-rownum]>td.schedule-slot-1, tr[data-rownum]>td.schedule-volunteerDriver")
-    .each((i,d) => $(d).css("border-right","2px dashed lightgray"));
+  $("tr[data-rownum]>td.schedule-slot-1, tr[data-rownum]>td.schedule-volunteerDriver")
+    .css("border-right","2px dashed lightgray");
 
-  // $("tr[data-rownum]>td")
-  //     .filter((i,d) => d.innerText == slotAvailableText)
-  //     .each((i,d) => $(d).css("background-color","green"));
+  // mark days with selected shifts
+  $("tr[data-rownum]")
+    .filter((i,tr) => $(tr).find("td.schedule-me:not(.schedule-point-events)").length > 0 )
+    .css("background-color", "lightyellow");
 
 });
